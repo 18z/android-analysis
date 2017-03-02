3e978c96e563bdd87ad8e1005132599642f73765
18z <xspiritualx@gmail.com>
2017-03-02 14:46:01 +0800

    first commit

---
+++ env-setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install apktool
+
+wget https://github.com/skylot/jadx/releases/download/v0.6.1/jadx-0.6.1.zip
+unzip jadx-0.6.1.zip -d j
+
+sudo easy_install ipython
+sudo easy_install pygments
+git clone https://github.com/androguard/androguard.git
+cd androguard
+sudo python setup.py install
